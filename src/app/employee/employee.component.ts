cff32aec2ee5e8db916c7cadd9efd522f5f831c1
Nishant <nishant.nln@tieto.com>
2020-06-12 13:45:20 +0530

    validations

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { CustomValidators } from '../shared/CustomValidators';
 
 @Component({
   selector: 'app-employee',
@@ -9,34 +16,153 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class EmployeeComponent implements OnInit {
   employeeForm: FormGroup;
 
-  constructor() {}
+  validationMessages = {
+    name: {
+      required: 'Name is required',
+      minlength: 'Name must be greater than 2 characters',
+      maxlength: 'Name must be less than 6 characters',
+    },
+
+    email: {
+      required: 'Email is required',
+      emailDomain: 'Domain value must be dell.com',
+    },
+    confirmEmail: {
+      required: 'Confirm Email is required',
+      //emailDomain: 'Domain value must be dell.com',
+    },
+    emailGroup: {
+      emailMismatch: 'Email & Confirmation Email do not match',
+    },
+    phone: {
+      required: 'Phone is required',
+    },
+    skillName: {
+      required: 'Skill is required',
+    },
+    experienceInYears: {
+      required: 'Experience  is required',
+    },
+    proficiency: {
+      required: 'Proficiency  is required',
+    },
+  };
+
+  formsError = {
+    name: '',
+    email: '',
+    confirmEmail: '',
+    emailGroup: '',
+    phone: '',
+    skillName: '',
+    experienceInYears: '',
+    proficiency: '',
+  };
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.employeeForm = new FormGroup({
-      name: new FormControl(),
-      email: new FormControl(),
-      skills: new FormGroup({
-        skillName: new FormControl(),
-        experienceInYears: new FormControl(),
-        proficiency: new FormControl(),
+    // this.employeeForm = new FormGroup({
+    //   name: new FormControl(),
+    //   email: new FormControl(),
+    //   skills: new FormGroup({
+    //     skillName: new FormControl(),
+    //     experienceInYears: new FormControl(),
+    //     proficiency: new FormControl(),
+    //   }),
+    // });
+
+    this.employeeForm = this.fb.group({
+      name: [
+        '',
+        [Validators.required, Validators.minLength(2), Validators.maxLength(6)],
+      ],
+      contactPreference: ['email'],
+      emailGroup: this.fb.group(
+        {
+          email: [
+            '',
+            [Validators.required, CustomValidators.emailDomain('dell.com')],
+          ],
+          confirmEmail: ['', Validators.required],
+        },
+        { validator: CustomValidators.matchEmail }
+      ),
+      phone: [''],
+      skills: this.fb.group({
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required],
       }),
     });
+
+    this.employeeForm.valueChanges.subscribe((data) => {
+      this.logValidationErrors(this.employeeForm);
+    });
+
+    this.employeeForm
+      .get('contactPreference')
+      .valueChanges.subscribe((data: string) => {
+        this.onContactPreferenceChanged(data);
+      });
   }
+
+  onContactPreferenceChanged = (selectedValue: string) => {
+    const phoneControl = this.employeeForm.get('phone');
+
+    switch (selectedValue) {
+      case 'email':
+        phoneControl.clearValidators();
+        break;
+      case 'phone':
+        phoneControl.setValidators(Validators.required);
+        break;
+    }
+
+    phoneControl.updateValueAndValidity();
+  };
 
   onSubmit = () => {
     console.log(this.employeeForm.controls.name.value);
     console.log(this.employeeForm.get('name').value);
   };
 
-  onLoadDataClick = (): void => {
-    this.employeeForm.patchValue({
-      name: 'Ruu',
-      email: 'ru@tieto.com',
-      // skills: {
-      //   skillName: 'HTML',
-      //   experienceInYears: 1,
-      //   proficiency: 'beginner',
-      // },
+  logValidationErrors = (group: FormGroup = this.employeeForm) => {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      //abstractControl.markAsDirty();
+      this.formsError[key] = '';
+      if (
+        abstractControl &&
+        !abstractControl.valid &&
+        (abstractControl.touched || abstractControl.dirty)
+      ) {
+        const messages = this.validationMessages[key];
+        for (const errorKey in abstractControl.errors) {
+          this.formsError[key] += messages[errorKey] + ' ';
+        }
+      }
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+      }
     });
   };
+
+  onLoadDataClick = (): void => {
+    // this.employeeForm.patchValue({
+    //   name: 'Ruu',
+    //   email: 'ru@tieto.com',
+    //   // skills: {
+    //   //   skillName: 'HTML',
+    //   //   experienceInYears: 1,
+    //   //   proficiency: 'beginner',
+    //   // },
+    // });
+    this.logValidationErrors();
+    //console.log(this.formsError);
+  };
+
+  get name() {
+    return this.employeeForm.get('name');
+  }
 }
---
+++ src/app/shared/CustomValidators.ts
@@ -0,0 +1,34 @@
+import { AbstractControl } from '@angular/forms';
+export class CustomValidators {
+  static emailDomain = (domainName: string) => {
+    return (
+      control: AbstractControl
+    ): {
+      [key: string]: boolean;
+    } | null => {
+      const email: string = control.value;
+      const domain = email.substring(email.lastIndexOf('@') + 1);
+      if (
+        email === '' ||
+        domain.toLocaleLowerCase() === domainName.toLocaleLowerCase()
+      ) {
+        return null;
+      } else {
+        return { emailDomain: true };
+      }
+    };
+  };
+
+  static matchEmail = (
+    group: AbstractControl
+  ): { [key: string]: any } | null => {
+    const emailControl = group.get('email');
+    const confirmEmailControl = group.get('confirmEmail');
+
+    if (emailControl.value === confirmEmailControl.value) {
+      return null;
+    } else {
+      return { emailMismatch: true };
+    }
+  };
+}
